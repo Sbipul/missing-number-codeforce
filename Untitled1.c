01dd35788b9b6dc4fd9e190c918a9faf665c05b4
Sbipul <bipulcroy121@gmail.com>
2022-11-13 21:31:35 +0600

    solved

---
+++ Untitled1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+
+    int N,x;
+    int arr[200001];
+    int i;
+
+    scanf("%d",&N);
+    for(i=1;i<N;i++){
+        scanf("%d",&x);
+        arr[x]=1;
+    };
+
+    for(i=1;i<N;i++){
+        printf("Testing %d position\n",i);
+        if(arr[i]==0){
+            printf("%d\n",i);
+            break;
+        };
+    };
+
+    return 0;
+};
